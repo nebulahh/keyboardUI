863b2c238dc769c0ce726181d2fd7291abaf35b6
Lekan <olalekancrown11@gmail.com>
2023-02-13 21:19:34 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'some-color': '#f6f6f6',
+        'text-color': '#5abeb3',
+      },
+      boxShadow: {
+        'button': '5px 5px 10px #bebebe -5px -5px 10px #ffffff',
+      },
+      gap: {
+        '11': '0.80rem',
+      }
+    },
+  },
+  plugins: [],
+}
